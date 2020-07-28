b28afab6960d6f449ed524adfd7211848908086a
syugen <syugen11@gmail.com>
2020-07-29 00:11:57 +0900

    Initial Commit

---
+++ Countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by 齋藤就元 on 2020/07/29.
+//  Copyright © 2020 Saito Syugen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
